e844736a8bef63acef705915a8090117ce79a64b
Aaron Gussman <agussman@thehumangeo.com>
2018-02-20 11:39:36 -0500

    Got it working as MatSidenavModule, but realize it probably doesn't need to be its own component

--- hotlanesbot-ng5/src/app/material.module.ts
+++ hotlanesbot-ng5/src/app/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule],
+  imports: [MatButtonModule, MatToolbarModule, MatSidenavModule],
+  exports: [MatButtonModule, MatToolbarModule, MatSidenavModule],
 })
 export class MaterialModule { }
