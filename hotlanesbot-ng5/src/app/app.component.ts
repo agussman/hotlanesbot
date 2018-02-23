5ba2387fb40f5ce5ac5d8baa301ccf26edc5802a
Aaron Gussman <agussman@thehumangeo.com>
2018-02-22 23:48:59 -0500

    Added map layers (because I needed a commit)

--- hotlanesbot-ng5/src/app/app.component.ts
+++ hotlanesbot-ng5/src/app/app.component.ts
@@ -21,4 +21,15 @@ export class AppComponent {
     center: latLng([ 38.8427869, -77.2375653 ])
   };
 
+  layersControl = {
+      baseLayers: {
+          'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+          'Google': tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', { maxZoom: 18, attribution: '...'}),
+          'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      },
+      overlays: {
+         // 'Big Circle': circle([ 46.95, -122 ], { radius: 5000 }),
+         // 'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
+      }
+  }
 }
